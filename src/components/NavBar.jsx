7164aaa423bf64b57e668a9c244ca6b4ac10295a
Mahamed <122625156+Mahamed507@users.noreply.github.com>
2026-04-05 17:40:19 -0500

    Add files via upload

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom'
+import '../css/Navbar.css'
+
+function NavBar(){
+
+    return(
+        <div className={'navbar'}>
+
+            <div className={'navbar-brand'}>
+
+                <Link to={'/'}>Movie App</Link>
+
+
+            </div>
+
+            <div className={'navbar-links'}>
+                <Link to={'/'} className={'nav-link'}>Home</Link>
+                <Link to={'/favorites'} className={'nav-link'}>Favorite</Link>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+const API_KEY = "06722f16a54ef7a3f6a5c74f8a7c8cc9";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+
+    const data = await response.json()
+
+    return data.results
+}
+
+
+export const searchMovies = async (query) => {
+
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+            query
+        )}`
+    );
+
+    const data = await response.json()
+
+    return data.results
+}
